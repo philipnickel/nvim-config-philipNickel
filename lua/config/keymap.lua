41b103c373abf935337b6d5ffdae03d35a7f3aff
Jannik Buhr <jannik.m.buhr@gmail.com>
2025-08-04 15:41:30 +0200

    dev

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -179,16 +179,36 @@ end
 --- Insert code chunk of given language
 --- Splits current chunk if already within a chunk
 --- @param lang string
-local insert_code_chunk = function(lang)
+--- @param curly boolean
+local insert_a_code_chunk = function(lang, curly)
   vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<esc>', true, false, true), 'n', true)
   local keys
+  if curly == nil then
+    curly = true
+  end
   if is_code_chunk() then
-    keys = [[o```<cr><cr>```{]] .. lang .. [[}<esc>o]]
+    if curly then
+      keys = [[o```<cr><cr>```{]] .. lang .. [[}<esc>o]]
+    else
+      keys = [[o```<cr><cr>```]] .. lang .. [[<esc>o]]
+    end
   else
-    keys = [[o```{]] .. lang .. [[}<cr>```<esc>O]]
+    if curly then
+      keys = [[o```{]] .. lang .. [[}<cr>```<esc>O]]
+    else
+      keys = [[o```]] .. lang .. [[<cr>```<esc>O]]
+    end
   end
   keys = vim.api.nvim_replace_termcodes(keys, true, false, true)
   vim.api.nvim_feedkeys(keys, 'n', false)
+end
+
+local insert_code_chunk = function(lang)
+  insert_a_code_chunk(lang, true)
+end
+
+local insert_plain_code_chunk = function(lang)
+  insert_a_code_chunk(lang, false)
 end
 
 local insert_r_chunk = function()
@@ -214,6 +234,36 @@ end
 local insert_ojs_chunk = function()
   insert_code_chunk 'ojs'
 end
+
+
+local insert_plain_r_chunk = function()
+  insert_plain_code_chunk 'r'
+end
+
+local insert_plain_py_chunk = function()
+  insert_plain_code_chunk 'python'
+end
+
+local insert_plain_lua_chunk = function()
+  insert_plain_code_chunk 'lua'
+end
+
+local insert_plain_julia_chunk = function()
+  insert_plain_code_chunk 'julia'
+end
+
+local insert_plain_bash_chunk = function()
+  insert_plain_code_chunk 'bash'
+end
+
+local insert_plain_ojs_chunk = function()
+  insert_plain_code_chunk 'ojs'
+end
+
+
+
+
+
 
 --show kepbindings with whichkey
 --add your own here if you want them to
@@ -393,14 +443,23 @@ wk.add({
     { '<leader>lg', ':Neogen<cr>', desc = 'neo[g]en docstring' },
     { '<leader>o', group = '[o]tter & c[o]de' },
     { '<leader>oa', require('otter').activate, desc = 'otter [a]ctivate' },
-    { '<leader>ob', insert_bash_chunk, desc = '[b]ash code chunk' },
     { '<leader>oc', 'O# %%<cr>', desc = 'magic [c]omment code chunk # %%' },
     { '<leader>od', require('otter').activate, desc = 'otter [d]eactivate' },
+
     { '<leader>oj', insert_julia_chunk, desc = '[j]ulia code chunk' },
     { '<leader>ol', insert_lua_chunk, desc = '[l]lua code chunk' },
     { '<leader>oo', insert_ojs_chunk, desc = '[o]bservable js code chunk' },
     { '<leader>op', insert_py_chunk, desc = '[p]ython code chunk' },
     { '<leader>or', insert_r_chunk, desc = '[r] code chunk' },
+    { '<leader>ob', insert_bash_chunk, desc = '[b]ash code chunk' },
+
+    { '<leader>Oj', insert_plain_julia_chunk, desc = '[j]ulia code chunk' },
+    { '<leader>Ol', insert_plain_lua_chunk, desc = '[l]lua code chunk' },
+    { '<leader>Oo', insert_plain_ojs_chunk, desc = '[o]bservable js code chunk' },
+    { '<leader>Op', insert_plain_py_chunk, desc = '[p]ython code chunk' },
+    { '<leader>Or', insert_plain_r_chunk, desc = '[r] code chunk' },
+    { '<leader>Ob', insert_plain_bash_chunk, desc = '[b]ash code chunk' },
+
     { '<leader>q', group = '[q]uarto' },
     {
       '<leader>qE',
--- lua/plugins/quarto.lua
+++ lua/plugins/quarto.lua
@@ -130,7 +130,7 @@ return {
   {
     'benlubas/molten-nvim',
     dev = false,
-    enabled = true,
+    enabled = false,
     version = '^1.0.0', -- use version <2.0.0 to avoid breaking changes
     build = ':UpdateRemotePlugins',
     init = function()
