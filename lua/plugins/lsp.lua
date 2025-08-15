ff4e159682f69b4a2a9afa5d3e54739feb243a2b
Jannik Buhr <jannik.m.buhr@gmail.com>
2025-08-15 12:31:09 +0200

    don't autoenable with mason lspconfig

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -21,6 +21,7 @@ return {
       {
         'mason-org/mason-lspconfig.nvim',
         opts = {
+          automatic_enable = false,
           ensure_installed = {
             'lua_ls',
             'bashls',
